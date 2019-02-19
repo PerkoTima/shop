fcc342262774a4197baff72cbd5eb5ca35a9daf5
PerkoTima <perko.t@mail.ru>
2019-02-20 01:20:38 +0300

    last cmt

---
+++ js/index.js
@@ -0,0 +1,20 @@
+function menu(){
+		document.getElementById("menu-active").classList.toggle("menu-active-on");
+}	
+	function price(){
+		document.getElementById("price-active").classList.toggle("price-active");
+		document.getElementById("price-arrow").classList.toggle("flaticon-down-arrow");
+	}
+
+	function brand(){
+		document.getElementById("brand-active").classList.toggle("brand-active");
+		document.getElementById("brand-arrow").classList.toggle("flaticon-down-arrow");
+	}
+	function size(){
+		document.getElementById("size-active").classList.toggle("size-active");
+		document.getElementById("size-arrow").classList.toggle("flaticon-down-arrow");
+	}
+	function color(){
+		document.getElementById("color-active").classList.toggle("color-active");
+		document.getElementById("color-arrow").classList.toggle("flaticon-down-arrow");	
+	}
